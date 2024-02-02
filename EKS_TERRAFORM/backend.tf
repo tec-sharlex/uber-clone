9876a51a0f5969dd78a39437d0bffeced7d4337d
frankgunz <franktsopmo@gmail.com>
2024-02-01 21:41:09 -0600

    change S3 bucket

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bucket0097" # Replace with your actual S3 bucket name
+    bucket = "game24" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
