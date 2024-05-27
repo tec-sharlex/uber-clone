1229051e48429309bc144f5b5dbdbc2ec6a32503
tec-sharlex <fomenarivaldo123@gmail.com>
2024-05-26 20:55:02 -0400

    modifying s3 bucket

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-ownS3" # Replace with your actual S3 bucket name
+    bucket = "my-owns3" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
